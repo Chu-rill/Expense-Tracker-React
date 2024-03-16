d6eb944cb39ed5612f8bc04ab88be6584b4722b0
Churchill <churchillrimamskep@gmail.com>
2024-03-16 09:13:28 +0100

    made the basic state management

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddTransaction() {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
   return (
     <>
       <h3>Add New Transaction</h3>
-      <form id="form">
+      <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
+          <button className="btn">Add Transaction</button>
         </div>
       </form>
     </>
